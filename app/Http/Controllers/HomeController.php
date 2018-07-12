2d431ce2378d9b87144f0024a7a6e871e32fdc19
Unknown <braydendiluca@gmail.com>
2018-07-12 22:49:23 +0700

    added 'inspirational' messages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Exercise;
 use App\WorkoutSet;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -27,8 +28,24 @@ class HomeController extends Controller
     {
         $exercises = Exercise::all();
         $sets = WorkoutSet::all();
+        $message = null;
+        //Check time since last logged set
+        $last_set = WorkoutSet::orderBy('created_at', 'desc')->first();
+        // $last_set = $last_set['created_at'];
+        $last_set = $last_set->created_at;
+        $now = Carbon::now();
+        $diff = $now->diffInDays($last_set);
+        //set alerts for 1 week, 1 month
+        if($diff > 62){
+          $message = "Look I know I was a bit rude, but seriously, get back to the gym. You'll feel great I promise!";
+        }else if($diff > 31){
+          $message = "Seriously?!?! It's been over a month. Get your fat fucking ass back in the gym!";
+        }else if($diff > 7){
+          $message = "You haven't logged a set in over a week! Don't be a pussy, get back in the gym!";
+        }
         return view('home')
           ->with('exercises', $exercises)
-          ->with('sets', $sets);
+          ->with('sets', $sets)
+          ->with('message', $message);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md">
+          @if($message != null)
+            <div class="alert alert-danger" role="alert">
+              {{  $message  }}
+            </div>
+          @endif
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
