417c5f883c667bfbdb11b9f9fc3a93ee3a45fae8
Unknown <braydendiluca@gmail.com>
2018-10-04 23:03:35 +1000

    Reordered exercises on dashboard
    Exercises are ordered by latest set. Doesn't matter if the set is a warmup or not.
    Only real sets are presented however.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,7 +53,7 @@
                   </tr>
                 </thead>
                 <tbody>
-                  @foreach($exercise->take(5) as $set)
+                  @foreach($exercise->where('warmup', '0')->take(5) as $set)
                     @if($set->warmup == 0)
                       <tr>
                           <td>{{  $set->created_at->toFormattedDateString()  }}</td>
