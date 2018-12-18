5aff6f0a1fb9c3b7ebedcc66633125a13677827e
Unknown <braydendiluca@gmail.com>
2018-12-18 20:17:05 +1100

    added loading animation

--- resources/assets/js/ajax.js
+++
@@ -1,9 +0,0 @@
-$('#card-populate').on('click', function(){
-  $.get('ajax/exercise', function(data){
-    $('#card-data').html(data);
-  });
-});
-
-
-
-
--- resources/assets/js/components/Dashboard.js
+++ resources/assets/js/components/Dashboard.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import ExerciseCard from './ExerciseCard';
 import axios from 'axios';
+import ReactLoading from 'react-loading';
 
 export default class Dashboard extends Component{
     constructor(props){
         super(props);
         this.state = {
           exercises: {}
+            loading: true,
         };
         this.updateDashboard();
     }
@@ -18,7 +20,10 @@ export default class Dashboard extends Component{
                 console.log(res.data);
                 const cards = res.data.map((obj, i) => <ExerciseCard exercise={obj} key={i} updater={this.updateDashboard}/>);
                 console.log(cards);
-                this.setState({ cards: cards });
+                this.setState({
+                    cards: cards,
+                    loading: false
+                    });
             });
     }
 
@@ -30,17 +35,12 @@ export default class Dashboard extends Component{
         return(
             <div className="container">
                 <div className="row">
-                    <div className="col-md">
-                        <div id="modalDiv">
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
                     <div className="col-md text-center">
                         <h2 className="display-2">Recent Workouts</h2>
                     </div>
                 </div>
-                <div className="row">
+                <div className="row text-center">
+                    <ReactLoading type="balls" color="#555555" height="5%" width="100%" hidden={!this.state.loading} />
                     {this.state.cards}
                 </div>
             </div>
