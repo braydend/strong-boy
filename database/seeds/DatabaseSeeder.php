a20962c754050d67f36d49579ed8fc7d0c8eba8c
Unknown <braydendiluca@gmail.com>
2018-12-31 20:25:39 -0500

    Created seed for test account
    Added check with console feedback if the test account is already active or app is in a production environment

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //Create the test account if in dev environment
+        if(getenv('APP_DEBUG')) {
+            $this->call(TestAccountSeeder::class);
+        }else{
+            print ("YOU ARE IN A PRODUCTION ENVIRONMENT. IF YOU WISH TO CREATE A TEST ACCOUNT, YOU MUST DO SO IN THE APPLICATION");
+        }
     }
 }
