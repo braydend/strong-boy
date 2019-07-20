c2d1c016c6f5d2a6c4dd3c59b0c01096bf4c46bd
Unknown <braydendiluca@gmail.com>
2019-07-20 15:22:52 +1000

    Lint all the things

--- resources/assets/js/components/NavItem.js
+++ resources/assets/js/components/NavItem.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 export default class NavItem extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            name: this.props.name,
-            link: this.props.link
-        }
-    }
+	constructor(props){
+		super(props);
+		this.state = {
+			name: this.props.name,
+			link: this.props.link
+		};
+	}
 
-    render() {
-        return (
-            <a href={this.state.link}>
-                <div className="nav-item">
-                    <h2>{this.state.name}</h2>
-                </div>
-            </a>
-        );
-    }
+	render() {
+		return (
+			<a href={this.state.link}>
+				<div className="nav-item">
+					<h2>{this.state.name}</h2>
+				</div>
+			</a>
+		);
+	}
 }
 
--- resources/assets/js/components/TopPanel.js
+++ resources/assets/js/components/TopPanel.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import {Col, Row} from "react-bootstrap";
 
 export default class TopPanel extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            context: this.props.pageName,
-            user: this.props.user
-        };
-    }
+	constructor(props){
+		super(props);
+		this.state = {
+			context: this.props.pageName,
+			user: this.props.user
+		};
+	}
 
-    render() {
-        return (
-            <div className="dashboard-top-panel">
-                <Row>
-                    <Col md={12} lg={4}>
-                        <div className="brand">
+	render() {
+		return (
+			<div className="dashboard-top-panel">
+				<Row>
+					<Col md={12} lg={4}>
+						<div className="brand">
                             Strongr
-                        </div>
-                    </Col>
-                    <Col md={12} lg={4}>
-                        <div className="context">
-                            {this.state.context}
-                        </div>
-                    </Col>
-                    <Col md={12} lg={4}>
-                        <div className="auth">
-                            <span className="icon">
+						</div>
+					</Col>
+					<Col md={12} lg={4}>
+						<div className="context">
+							{this.state.context}
+						</div>
+					</Col>
+					<Col md={12} lg={4}>
+						<div className="auth">
+							<span className="icon">
                                 icon
-                            </span>
-                            {this.state.user}
-                        </div>
-                    </Col>
-                </Row>
-            </div>
-        );
-    }
+							</span>
+							{this.state.user}
+						</div>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
